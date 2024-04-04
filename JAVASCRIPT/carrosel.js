646b1fe63b8ea5b7dff3e15037d0fd4c45b7ab6e
FranciscoVicente2309 <francisco.vicente2309@gmail.com>
2024-04-04 19:33:59 -0300

    att home

--- JAVASCRIPT/carrosel.js
+++ JAVASCRIPT/carrosel.js
@@ -1,25 +1,33 @@
-const controle = document.querySelectorAll(".controle");
+const controls = document.querySelectorAll(".control");
 let currentItem = 0;
-const item = document.querySelectorAll(".item");
-const maxItems = item.length;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
 
-controle.forEach(controle => {
-    controle.addEventListener("click", () => {
-        const esquerda = controle.classList.contains("left");
-        if(esquerda) {
-            currentItem -= 1;
-        } else {
-            currentItem += 1;
-        }
-        if(currentItem >= maxItems) {
-            currentItem = 0
-        }
-        if(currentItem < 0) {
-            currentItem = maxItems - 1
-        }
-        itens.foreach(item => item.classList.remove("current_item"))
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
     });
-    itens[currentItem].scrollIntoView({
-        inline : "center"
-    })
+
+    items[currentItem].classList.add("current-item");
+  });
 });

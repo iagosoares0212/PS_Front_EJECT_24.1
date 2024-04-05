20cf4d67abb4c1a8da1ab8a1077c82b260ca3049
iagosoares0212 <iagosoares55@gmail.com>
2024-04-04 21:45:15 -0300

    sliders ultimas mudaças

--- JS/sobre.js
+++ JS/sobre.js
@@ -1,15 +1,33 @@
-let count = 1;
+// Slider 1
+let count1 = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function(){
-    nextImage();
+    nextImage1();
 }, 2000);
 
-function nextImage(){
-    count++;
-    if(count > 5){  
-        count = 1;
+function nextImage1(){
+    count1++;
+    if(count1 > 5){  
+        count1 = 1;
     }
 
-    document.getElementById("radio" + count).checked = true;
+    document.getElementById("radio" + count1).checked = true;
+}
+
+// Slider 2
+let count2 = 1;
+document.getElementById("radio6").checked = true;
+
+setInterval(function(){
+    nextImage2();
+}, 2000);
+
+function nextImage2(){
+    count2++;
+    if(count2 > 5){  
+        count2 = 1;
+    }
+
+    document.getElementById("radio" + (count2 + 5)).checked = true;
 }

1774103d21509640fb5b7d0fb63f52ff641d79a5
FranciscoVicente2309 <francisco.vicente2309@gmail.com>
2024-04-07 14:49:54 -0300

    att home

--- JAVASCRIPT/teste.js
+++ JAVASCRIPT/teste.js
@@ -1,33 +1,33 @@
-const controls_esquerda = document.querySelectorAll(".control_esquerda");
-let currentItem_esquerda = 0;
-const items_esquerda = document.querySelectorAll(".item_esquerda");
-const maxItems_esquerda = items_esquerda.length;
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
 
-controls_esquerda.forEach((control_esquerda) => {
-  control_esquerda.addEventListener("click", (e) => {
-    isLeft_esquerda = e.target.classList.contains("arrow_left_esquerda");
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
 
-    if (isLeft_esquerda) {
-      currentItem_esquerda -= 1;
+    if (isLeft) {
+      currentItem -= 1;
     } else {
-      currentItem_esquerda += 1;
+      currentItem += 1;
     }
 
-    if (currentItem_esquerda >= maxItems_esquerda) {
-      currentItem_esquerda = 0;
+    if (currentItem >= maxItems) {
+      currentItem = 0;
     }
 
-    if (currentItem_esquerda < 0) {
-      currentItem_esquerda = maxItems_esquerda - 1;
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
     }
 
-    items_esquerda.forEach((item_esquerda) => item_esquerda.classList.remove("current_item_esquerda"));
+    items.forEach((item) => item.classList.remove("current-item"));
 
-    items_esquerda[currentItem_esquerda].scrollIntoView({
+    items[currentItem].scrollIntoView({
       behavior: "smooth",
       inline: "center"
     });
 
-    items_esquerda[currentItem_esquerda].classList.add("current_item_esquerda");
+    items[currentItem].classList.add("current-item");
   });
 });
